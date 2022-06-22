b7d7abf3e75fddc86b67a8c7921e8ce62a36f705
nkr <nkgithubnarkuziev108@gmail.com>
2022-06-22 14:31:28 +0500

    updated logo's location

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.onload = () => {
 	const pics = ["logo-1", "logo-2", "logo-3", "logo-4"];
 	const idx = Math.floor(Math.random() * (pics.length - 1));
-	document.querySelector("#main-header > img").setAttribute("src", `src/${pics[idx]}.png`);
+	document.querySelector("#main-header > img").setAttribute("src", `src/logo/${pics[idx]}.png`);
 };
